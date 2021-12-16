f453dd088f7dbf8eacdc6a60c0cfdc9e7418761d
akelizer <96201786+akelizer@users.noreply.github.com>
2021-12-16 12:28:57 -0500

    adding all n2t files from local machine to git repo

---
+++ nand2tetris/projects/05/ScreenKbdTest.asm
@@ -0,0 +1,11 @@
+// program to test  the values of the last Screen register and the Keyboard
+// KBD = keyboard; SCREEN=first screen address register; Screen is 16384 - 24575
+// Keyboard is 24576
+
+@24575
+@24573
+@24574
+@8192
+D=M
+
+@KBD
